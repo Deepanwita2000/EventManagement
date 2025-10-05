127d9eb1a23881428c582e5252ca9546af46ef0d
Deepanwita2000 <85988268+Deepanwita2000@users.noreply.github.com>
2025-10-05 20:41:57 +0530

    Delete category directory

--- category/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns=[
-    path("api_all_category/", views.api_all_category, name="api_all_category"),
-    path('api_add_category/', views.api_add_category, name='api_add_category'),
-    # path("edit/<int:pk>/", views.api_update_stream, name="edit_stream"),
-    # path("delete/<int:stream_id>/", views.api_delete_stream, name="delete_stream"),
-]
