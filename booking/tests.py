1e60cc878d1e628301751b1fc7b9499beee79208
Deepanwita2000 <85988268+Deepanwita2000@users.noreply.github.com>
2025-10-05 20:41:46 +0530

    Delete booking directory

--- booking/tests.py
+++
@@ -1,9 +0,0 @@
-
-from django.test import TestCase
-
-# Create your tests here.
-# =======
-# from django.test import TestCase
-
-# # Create your tests here.
-# >>>>>>> 3c71da83ca8249c58b92e6741917a4e4aada0e7c
